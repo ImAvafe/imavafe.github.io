d38658f459a0c3324b71562442aeda0dde1dc35c
Avafe <65048459+ImAvafe@users.noreply.github.com>
2025-10-24 17:22:56 -0400

    Improve text sizing and spacing

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -10,7 +10,7 @@
 
 <div class="navbar bg-base-100 px-4 outline-1 outline-neutral">
 	<div class="flex w-full flex-wrap justify-between gap-2">
-		<a href="/" class="no-underline"><h2 class="mt-0 mb-0 text-primary">{title}</h2></a>
+		<a href="/" class="my-0 text-3xl font-bold text-primary no-underline">{title}</a>
 		<ul class="mt-0 mb-0 flex list-none flex-row flex-wrap gap-2 px-0">
 			{#each items as item}
 				<li class="mt-0 mb-0 p-0">
