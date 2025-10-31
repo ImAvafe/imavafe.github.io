7ac64f0687954a4276b4f0e7675c690b33e03c93
Avafe <65048459+ImAvafe@users.noreply.github.com>
2025-10-31 12:38:15 -0400

    Add corner border effect to Article

--- src/lib/components/Article.svelte
+++ src/lib/components/Article.svelte
@@ -11,7 +11,7 @@
 	<div
 		class="card flex flex-row gap-6 p-4 outline-1 outline-neutral transition-colors hover:outline-primary"
 	>
-		<img src={icon} alt="Article icon." class="aspect-square w-36" />
+		<img src={icon} alt="Article icon." class="icon aspect-square w-36" />
 		<article class="flex flex-col gap-2">
 			<h1 class="mt-0 mb-0 text-2xl font-bold">{title}</h1>
 			<p class="mt-0 mb-0 flex-1">{description}</p>
@@ -26,3 +26,20 @@
 		</article>
 	</div>
 </a>
+
+<style>
+	.icon {
+		--s: 25px; /* the size on the corner */
+		--t: 1px; /* the thickness of the border */
+		--g: 6px; /* the gap between the border and image */
+
+		padding: calc(var(--g) + var(--t));
+		outline: var(--t) solid var(--color-neutral); /* the color here */
+		outline-offset: calc(-1 * var(--t));
+		mask:
+			conic-gradient(at var(--s) var(--s), #0000 75%, #000 0) 0 0 / calc(100% - var(--s))
+				calc(100% - var(--s)),
+			conic-gradient(#000 0 0) content-box;
+		transition: 0.4s;
+	}
+</style>
