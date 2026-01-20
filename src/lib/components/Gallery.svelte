f5066630b44ac9b65176444362374e27d63f9521
Avafe <65048459+ImAvafe@users.noreply.github.com>
2026-01-20 13:11:00 -0500

    Set up gallery component

---
+++ src/lib/components/Gallery.svelte
@@ -0,0 +1,35 @@
+<script>
+	import { onMount } from 'svelte';
+	import PhotoSwipeLightbox from 'photoswipe/lightbox';
+	import 'photoswipe/style.css';
+	export let galleryID;
+	export let images;
+
+	onMount(() => {
+		let lightbox = new PhotoSwipeLightbox({
+			gallery: '#' + galleryID,
+			children: 'a',
+			pswpModule: () => import('photoswipe')
+		});
+		lightbox.init();
+	});
+</script>
+
+<div class="pswp-gallery columns-2 gap-4 md:columns-3" id={galleryID}>
+	{#each images as image (image)}
+		<a
+			href={image.largeURL}
+			data-pswp-width={image.width}
+			data-pswp-height={image.height}
+			target="_blank"
+			rel="noreferrer"
+			class="mb-4 block break-inside-avoid align-top"
+		>
+			<img
+				src={image.thumbnailURL}
+				alt=""
+				class="mt-0 mb-0 h-auto w-full cursor-pointer rounded-lg"
+			/>
+		</a>
+	{/each}
+</div>
