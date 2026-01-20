a81a08e0b59bb610366307f34d79216208e9f3cd
Avafe <65048459+ImAvafe@users.noreply.github.com>
2026-01-20 12:51:34 -0500

    Update about and misc details

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -8,7 +8,10 @@
 
 <div class="px-4 py-4 outline-1 outline-neutral">
 	<div class="flex w-full flex-wrap justify-between">
-		<p class="mt-0 mb-0">{description}</p>
+		<span class="flex flex-row gap-2"
+			><p class="mt-0 mb-0">{description}</p>
+			<img src="/img/blahaj.gif" alt="Blahaj" class="mt-0 mb-0 w-8" /></span
+		>
 		<ul class="mt-0 mb-0 flex list-none flex-row flex-wrap px-0">
 			{#each links as link}
 				<li class="mt-0 mb-0 p-0">
