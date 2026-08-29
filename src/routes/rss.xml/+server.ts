bf58be20613102cb6fc6121a32b94e8c66bdb41d
Avafe <65048459+ImAvafe@users.noreply.github.com>
2026-08-29 12:29:00 +0800

    Deno

--- src/routes/rss.xml/+server.ts
+++ src/routes/rss.xml/+server.ts
@@ -3,36 +3,36 @@ import { generateRssFeed } from "feedsmith";
 
 export const prerender = true;
 
-function postToItem(post) {
-  return {
-    title: post.title,
-    description: post.description,
-    pubDate: new Date(post.date),
-    icon: post.icon,
-    categories: (post.categories || []).map((category) => {
-      return { name: category };
-    }),
-    media: {
-      thumbnails: [{ url: post.icon }],
-    },
-    link: `https://avafe.me/blog/${post.slug}`,
-    guid: `https://avafe.me/blog/${post.slug}`,
-  };
+function postToItem(post: any) {
+	return {
+		title: post.title,
+		description: post.description,
+		pubDate: new Date(post.date),
+		icon: post.icon,
+		categories: (post.categories || []).map((category: any) => {
+			return { name: category };
+		}),
+		media: {
+			thumbnails: [{ url: post.icon }],
+		},
+		link: `https://avafe.me/blog/${post.slug}`,
+		guid: `https://avafe.me/blog/${post.slug}`,
+	};
 }
 
 export async function GET({ fetch }) {
-  const response = await fetch("/api/posts");
-  const posts: Article[] = await response.json();
+	const response = await fetch("/api/posts");
+	const posts: Article[] = await response.json();
 
-  return new Response(
-    generateRssFeed({
-      title: `Avafe's Ramblings`,
-      link: "https://avafe.me",
-      description: `Game developer, FOSS fanatic.`,
-      language: "en-US",
-      items: posts.map((post) => {
-        return postToItem(post);
-      }),
-    }),
-  );
+	return new Response(
+		generateRssFeed({
+			title: `Avafe's Ramblings`,
+			link: "https://avafe.me",
+			description: `Game developer, FOSS fanatic.`,
+			language: "en-US",
+			items: posts.map((post) => {
+				return postToItem(post);
+			}),
+		}),
+	);
 }
